6452bf795f0d0feab744a47ffaeea5e7e8cf8e83
Frank Casella <frankcasella17@gmail.com>
2019-02-01 12:51:47 -0500

    Added project

---
+++ The-Last-Yeehaw2.0/Assets/Scripts/PewController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PewController : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(0.0f, 0.0f, speed);
+    }
+}
---
+++ The-Last-Yeehaw2.0/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    public GameObject Pew;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(Pew, shotSpawn.position, shotSpawn.rotation);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        GetComponent<Rigidbody>().velocity = movement * speed;
+
+        GetComponent<Rigidbody>().position = new Vector3
+        (
+            Mathf.Clamp(GetComponent<Rigidbody>().position.x, -5.0f, 5.0f), 0.0f,
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z, -2.0f, 2.0f)
+        );
+    }
+}
